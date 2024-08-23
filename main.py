07e473f81a8527e08585543728282476da1e3279
Joshua Amarfio <53336997+Amarfio@users.noreply.github.com>
2024-08-23 03:53:12 +0000

    first code push

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas
+import turtle
+
+screen = turtle.Screen()
+image = "gh.gif"
+screen.addshape(image)
+turtle.shape(image)
+turtle.goto(int(6.6900581),int(-1.7809231))
+# turtle.write("Kumasi")
+turtle.goto(int(5.591203),int(-0.2621324))
+turtle.write("Accra")
+screen.exitonclick()
